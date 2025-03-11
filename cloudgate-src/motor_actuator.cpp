00a5ae1b8fb7fb85b12014b4d4cc31dea5adee40
tighe-ecc <tighe@eightcoast.com>
2025-03-10 22:24:23 -0700

    configureMotor now updates configs of all motors on gateway

--- cloudgate-src/motor_actuator.cpp
+++ cloudgate-src/motor_actuator.cpp
@@ -64,110 +64,109 @@ void configureMotor() {
                     << "    Model:" << Member[i - 1].Model.ModelStr
                     << "    Firmware:" << Member[i - 1].Model.FirmVer
                     << std::endl;
-    }
 
-    // Demo actuator config
-    bool bGetPara = true;
+        bool bSetPara = true;
 
-    if (bGetPara) {
-        // Initial Config
+        if (bSetPara) {
+            // Initial Config
 
-        // Obtain whether to power on Enable 0 = disable, 1 = Enable
-        err = SdkGetInitialConfig(g_GtwyHandle, CANid, 0, &unRxData);
-        if (err) {
-            std::cout << "CANid:" << CANid << "    SdkGetInitialConfig Fail!\n";
-            return;
-        } else {
-            std::cout << "CANid:" << CANid << "    Power-on Enable = " << unRxData << "\n";
+            // Obtain whether to power on Enable 0 = disable, 1 = Enable
+            err = SdkGetInitialConfig(g_GtwyHandle, CANid, 0, &unRxData);
+            if (err) {
+                std::cout << "CANid:" << CANid << "    SdkGetInitialConfig Fail!\n";
+                return;
+            } else {
+                std::cout << "CANid:" << CANid << "    Power-on Enable = " << unRxData << "\n";
+            }
+
+            // Get positive turn setting, 0 = CW, 1= CCW
+            err = SdkGetInitialConfig(g_GtwyHandle, CANid, 1, &unRxData);
+            if (err) {
+                std::cout << "CANid:" << CANid << "    SdkGetInitialConfig Fail!\n";
+                return;
+            } else {
+                std::cout << "CANid:" << CANid << "    Positive turn setting = " << unRxData << "\n";
+            }
+
+            // Acceleration and deceleration mode 0 = value, 1 = time
+            err = SdkSetInitialConfig(g_GtwyHandle, CANid, 4, 0, &unRxData);
+            if (err) {
+                std::cout << "CANid:" << CANid << "    SdkSetInitialConfig Fail!\n";
+                return;
+            } else {
+                std::cout << "CANid:" << CANid << "    Acceleration and deceleration mode = " << unRxData << "\n";
+            }
+
+            // Set acceleration/deceleration rate to that of gravity
+            err = SdkSetAcceleration(g_GtwyHandle, CANid, 130667*10, &unRxData);
+            if (err) {
+                std::cout << "CANid:" << CANid << "    SdkGetAcceleration Fail!\n";
+                return;
+            } else {
+                std::cout << "CANid:" << CANid << "    Acceleration rate = " << unRxData << "\n";
+            }
+            err = SdkSetDeceleration(g_GtwyHandle, CANid, 130667*10, &unRxData);
+            if (err) {
+                std::cout << "CANid:" << CANid << "    SdkGetAcceleration Fail!\n";
+                return;
+            } else {
+                std::cout << "CANid:" << CANid << "    Deceleration rate = " << unRxData << "\n";
+            }
+
+            // Information Enable
+
+            // P1 Port change notification 0 = disable, 1 = Enable.
+            err = SdkGetInformEnable(g_GtwyHandle, CANid, 0, &unRxData);
+            if (err) {
+                std::cout << "CANid:" << CANid << "    SdkGetInformEnable Fail!\n";
+                return;
+            }
+
+            // PTP movement in place notification 0 = disable, 1 = Enable;
+            err = SdkGetInformEnable(g_GtwyHandle, CANid, 8, &unRxData);
+            if (err) {
+                std::cout << "CANid:" << CANid << "    SdkGetInformEnable Fail!\n";
+                return;
+            }
+
+            // Motor Config Information
+
+            // Get subdivide[1.2.4.8.16]
+            err = SdkGetMotorConfig(g_GtwyHandle, CANid, 0, &unRxData);
+            if (err) {
+                std::cout << "CANid:" << CANid << "    SdkGetMotorConfig Fail!\n";
+                return;
+            }
+
+            // Get current [10...80]
+            err = SdkGetMotorConfig(g_GtwyHandle, CANid, 1, &unRxData);
+            if (err) {
+                std::cout << "CANid:" << CANid << "    SdkGetMotorConfig Fail!\n";
+                return;
+            }
+
+            // Get the standby current flow [0...100(%)]
+            err = SdkGetMotorConfig(g_GtwyHandle, CANid, 2, &unRxData);
+            if (err) {
+                std::cout << "CANid:" << CANid << "    SdkGetMotorConfig Fail!\n";
+                return;
+            }
+
+            // Obtain automatic power-on enable delay [0...60000(ms)]
+            err = SdkGetMotorConfig(g_GtwyHandle, CANid, 3, &unRxData);
+            if (err) {
+                std::cout << "CANid:" << CANid << "    SdkGetMotorConfig Fail!\n";
+                return;
+            }
+
+            // Enable
+            err = SdkSetMotorOn(g_GtwyHandle, CANid, 1, &unRxData);
+            if (err != 0) { std::cout << "CANid:" << CANid << "    SdkSetMotorOn Fail!\n"; return; }
+
+            // Set origin
+            err = SdkSetOrigin(g_GtwyHandle, CANid);
+            if (err != 0) { std::cout << "CANid:" << CANid << "    SdkSetOrigin Fail!\n"; return; }
         }
-
-        // Get positive turn setting, 0 = CW, 1= CCW
-        err = SdkGetInitialConfig(g_GtwyHandle, CANid, 1, &unRxData);
-        if (err) {
-            std::cout << "CANid:" << CANid << "    SdkGetInitialConfig Fail!\n";
-            return;
-        } else {
-            std::cout << "CANid:" << CANid << "    Positive turn setting = " << unRxData << "\n";
-        }
-
-        // Acceleration and deceleration mode 0 = value, 1 = time
-        err = SdkSetInitialConfig(g_GtwyHandle, CANid, 4, 0, &unRxData);
-        if (err) {
-            std::cout << "CANid:" << CANid << "    SdkSetInitialConfig Fail!\n";
-            return;
-        } else {
-            std::cout << "CANid:" << CANid << "    Acceleration and deceleration mode = " << unRxData << "\n";
-        }
-
-        // Set acceleration/deceleration rate to that of gravity
-        err = SdkSetAcceleration(g_GtwyHandle, CANid, 130667*10, &unRxData);
-        if (err) {
-            std::cout << "CANid:" << CANid << "    SdkGetAcceleration Fail!\n";
-            return;
-        } else {
-            std::cout << "CANid:" << CANid << "    Acceleration rate = " << unRxData << "\n";
-        }
-        err = SdkSetDeceleration(g_GtwyHandle, CANid, 130667*10, &unRxData);
-        if (err) {
-            std::cout << "CANid:" << CANid << "    SdkGetAcceleration Fail!\n";
-            return;
-        } else {
-            std::cout << "CANid:" << CANid << "    Deceleration rate = " << unRxData << "\n";
-        }
-
-        // Information Enable
-
-        // P1 Port change notification 0 = disable, 1 = Enable.
-        err = SdkGetInformEnable(g_GtwyHandle, CANid, 0, &unRxData);
-        if (err) {
-            std::cout << "CANid:" << CANid << "    SdkGetInformEnable Fail!\n";
-            return;
-        }
-
-        // PTP movement in place notification 0 = disable, 1 = Enable;
-        err = SdkGetInformEnable(g_GtwyHandle, CANid, 8, &unRxData);
-        if (err) {
-            std::cout << "CANid:" << CANid << "    SdkGetInformEnable Fail!\n";
-            return;
-        }
-
-        // Motor Config Information
-
-        // Get subdivide[1.2.4.8.16]
-        err = SdkGetMotorConfig(g_GtwyHandle, CANid, 0, &unRxData);
-        if (err) {
-            std::cout << "CANid:" << CANid << "    SdkGetMotorConfig Fail!\n";
-            return;
-        }
-
-        // Get current [10...80]
-        err = SdkGetMotorConfig(g_GtwyHandle, CANid, 1, &unRxData);
-        if (err) {
-            std::cout << "CANid:" << CANid << "    SdkGetMotorConfig Fail!\n";
-            return;
-        }
-
-        // Get the standby current flow [0...100(%)]
-        err = SdkGetMotorConfig(g_GtwyHandle, CANid, 2, &unRxData);
-        if (err) {
-            std::cout << "CANid:" << CANid << "    SdkGetMotorConfig Fail!\n";
-            return;
-        }
-
-        // Obtain automatic power-on enable delay [0...60000(ms)]
-        err = SdkGetMotorConfig(g_GtwyHandle, CANid, 3, &unRxData);
-        if (err) {
-            std::cout << "CANid:" << CANid << "    SdkGetMotorConfig Fail!\n";
-            return;
-        }
-
-        // Enable
-        err = SdkSetMotorOn(g_GtwyHandle, CANid, 1, &unRxData);
-        if (err != 0) { std::cout << "CANid:" << CANid << "    SdkSetMotorOn Fail!\n"; return; }
-
-        // Set origin
-        err = SdkSetOrigin(g_GtwyHandle, CANid);
-        if (err != 0) { std::cout << "CANid:" << CANid << "    SdkSetOrigin Fail!\n"; return; }
     }
 }
 
